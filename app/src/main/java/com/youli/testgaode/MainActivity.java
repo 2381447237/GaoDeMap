b3d524047dcb442198f2ddae4eee45f4a9afc48a
2381447237 <2381447237@qq.com>
2017-11-14 13:04:53 +0800

    首次提交

---
+++ app/src/main/java/com/youli/testgaode/MainActivity.java
@@ -0,0 +1,119 @@
+package com.youli.testgaode;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends CheckPermissionsActivity {
+//8E:62:D0:AA:C7:46:BD:49:B1:B2:1F:EE:3D:4A:AC:20:15:EB:F3:D0
+
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mLocationClient;
+
+    private TextView jDuTv,wDuTv,gDuTv;
+
+    //声明AMapLocationClientOption对象
+    public AMapLocationClientOption mLocationOption;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        jDuTv= (TextView) findViewById(R.id.tv_jdu);
+        wDuTv= (TextView) findViewById(R.id.tv_wdu);
+        gDuTv= (TextView) findViewById(R.id.tv_gdu);
+        //初始化定位
+        mLocationClient = new AMapLocationClient(getApplicationContext());
+        //设置定位回调监听
+        mLocationClient.setLocationListener(new AMapLocationListener() {
+            @Override
+            public void onLocationChanged(AMapLocation aMapLocation) {
+
+                if(aMapLocation!=null){
+
+                    if(aMapLocation.getErrorCode()==0){
+                        //可在其中解析amapLocation获取相应内容。
+
+                        aMapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
+                        aMapLocation.getLatitude();//获取纬度
+                        aMapLocation.getLongitude();//获取经度
+                        aMapLocation.getAccuracy();//获取精度信息
+                        aMapLocation.getAddress();//地址，如果option中设置isNeedAddress为false，则没有此结果，网络定位结果中会有地址信息，GPS定位不返回地址信息。
+                        aMapLocation.getCountry();//国家信息
+                        aMapLocation.getProvince();//省信息
+                        aMapLocation.getCity();//城市信息
+                        aMapLocation.getDistrict();//城区信息
+                        aMapLocation.getStreet();//街道信息
+                        aMapLocation.getStreetNum();//街道门牌号信息
+                        aMapLocation.getCityCode();//城市编码
+                        aMapLocation.getAdCode();//地区编码
+                        aMapLocation.getAoiName();//获取当前定位点的AOI信息
+                        aMapLocation.getBuildingId();//获取当前室内定位的建筑物Id
+                        aMapLocation.getFloor();//获取当前室内定位的楼层
+                        aMapLocation.getGpsAccuracyStatus();//获取GPS的当前状态
+                         //获取定位时间
+                        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                        Date date = new Date(aMapLocation.getTime());
+                        df.format(date);
+                        Log.e("2017/11/14","经度=="+aMapLocation.getLongitude()+"纬度=="+aMapLocation.getLatitude()+"高度=="+aMapLocation.getAltitude());
+                        jDuTv.setText("经度:"+aMapLocation.getLongitude());
+                        wDuTv.setText("纬度:"+aMapLocation.getLatitude());
+                        gDuTv.setText("高度:"+aMapLocation.getAltitude());
+                    }else{
+                        //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
+                        Log.e("AmapError","location Error, ErrCode:"
+                                + aMapLocation.getErrorCode() + ", errInfo:"
+                                + aMapLocation.getErrorInfo());
+                    }
+                    }
+
+                }
+        });
+
+        //初始化AMapLocationClientOption对象
+        mLocationOption = new AMapLocationClientOption();
+        //设置定位模式为AMapLocationMode.Hight_Accuracy，高精度模式。
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+
+//        //设置定位模式为AMapLocationMode.Battery_Saving，低功耗模式。
+//        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Battery_Saving);
+
+        //设置定位间隔,单位毫秒,默认为2000ms，最低1000ms。
+        mLocationOption.setInterval(1000);
+
+        //设置是否返回地址信息（默认返回地址信息）
+        mLocationOption.setNeedAddress(true);
+
+        //设置是否允许模拟位置,默认为true，允许模拟位置
+        mLocationOption.setMockEnable(true);
+
+        //单位是毫秒，默认30000毫秒，建议超时时间不要低于8000毫秒。
+        mLocationOption.setHttpTimeOut(20000);
+
+        //关闭缓存机制
+        mLocationOption.setLocationCacheEnable(false);
+
+        //给定位客户端对象设置定位参数
+        mLocationClient.setLocationOption(mLocationOption);
+         //启动定位
+        mLocationClient.startLocation();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mLocationClient.stopLocation();//停止定位后，本地定位服务并不会被销毁
+        mLocationClient.onDestroy();//销毁定位客户端，同时销毁本地定位服务。
+    }
+}
